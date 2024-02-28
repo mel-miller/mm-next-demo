f877e215bc24523ace8bf5040de36330d15fe0d2
M Miller <melissa.miller@pantheon.io>
2024-02-27 20:15:36 -0800

    Revert "Update docs-sample-text.js"
    This reverts commit 8c5f9b0494ee072de1ed59b1b01c639da2b47e0d.

--- src/app/docs/docs-sample-text.js
+++ src/app/docs/docs-sample-text.js
@@ -4,9 +4,21 @@ export const docsSampleText = (
       This is an introduction to the document. It should be a brief overview of what the document is about.
     </p>
     <h2 id='section-1'>Section 1</h2>
-    <p>Section 1 is about the following things.</p>
+    <p>
+      Section 1 is about the following things. A baby photo booth ennui pabst YOLO, organic migas trust fund gochujang
+      sartorial wolf. Cold-pressed sus tumeric, vaporware palo santo next level succulents church-key bicycle rights
+      intelligentsia single-origin coffee. Freegan ethical PBR&B tumblr. Twee af stumptown marxism umami. Yuccie ethical
+      cronut jawn fit pug photo booth flannel neutral milk hotel marxism raw denim. Leggings actually PBR&B air plant
+      try-hard tumblr, seitan celiac same cliche hella hammock ramps.
+    </p>
     <h3>Section 1a</h3>
-    <p>Section 1a digs deeper into the following things.</p>
+    <p>
+      Section 1a digs deeper into the following things. A baby photo booth ennui pabst YOLO, organic migas trust fund
+      gochujang sartorial wolf. Cold-pressed sus tumeric, vaporware palo santo next level succulents church-key bicycle
+      rights intelligentsia single-origin coffee. Freegan ethical PBR&B tumblr. Twee af stumptown marxism umami. Yuccie
+      ethical cronut jawn fit pug photo booth flannel neutral milk hotel marxism raw denim. Leggings actually PBR&B air
+      plant try-hard tumblr, seitan celiac same cliche hella hammock ramps.
+    </p>
     <h3 id='section-1b.2'>Section 1b</h3>
     <p>Section 1b contains a list of things.</p>
     <ul>
@@ -16,8 +28,27 @@ export const docsSampleText = (
       <li>Marxism raw denim</li>
     </ul>
     <h2>Section 2</h2>
-    <p>Section 2 is about the following things.</p>
+    <p>
+      Section 2 is about the following things. A baby photo booth ennui pabst YOLO, organic migas trust fund gochujang
+      sartorial wolf. Cold-pressed sus tumeric, vaporware palo santo next level succulents church-key bicycle rights
+      intelligentsia single-origin coffee. Freegan ethical PBR&B tumblr. Twee af stumptown marxism umami. Yuccie ethical
+      cronut jawn fit pug photo booth flannel neutral milk hotel marxism raw denim. Leggings actually PBR&B air plant
+      try-hard tumblr, seitan celiac same cliche hella hammock ramps.
+    </p>
     <h2 id='section-3'>Section 3</h2>
-    <p>And finally, Section 3 is about the following things.</p>
+    <p>
+      And finally, Section 3 is about the following things. A baby photo booth ennui pabst YOLO, organic migas trust
+      fund gochujang sartorial wolf. Cold-pressed sus tumeric, vaporware palo santo next level succulents church-key
+      bicycle rights intelligentsia single-origin coffee. Freegan ethical PBR&B tumblr. Twee af stumptown marxism umami.
+      Yuccie ethical cronut jawn fit pug photo booth flannel neutral milk hotel marxism raw denim. Leggings actually
+      PBR&B air plant try-hard tumblr, seitan celiac same cliche hella hammock ramps.
+    </p>
+    <p>
+      A baby photo booth ennui pabst YOLO, organic migas trust fund gochujang sartorial wolf. Cold-pressed sus tumeric,
+      vaporware palo santo next level succulents church-key bicycle rights intelligentsia single-origin coffee. Freegan
+      ethical PBR&B tumblr. Twee af stumptown marxism umami. Yuccie ethical cronut jawn fit pug photo booth flannel
+      neutral milk hotel marxism raw denim. Leggings actually PBR&B air plant try-hard tumblr, seitan celiac same cliche
+      hella hammock ramps.
+    </p>
   </>
 );
