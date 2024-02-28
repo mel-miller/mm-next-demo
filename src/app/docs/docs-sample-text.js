ce74f509a118924d40c69e87ce64682937628c0a
M Miller <melissa.miller@pantheon.io>
2024-02-27 17:18:37 -0800

    Update docs-sample-text.js

--- src/app/docs/docs-sample-text.js
+++ src/app/docs/docs-sample-text.js
@@ -13,14 +13,14 @@ export const docsSampleText = (
     </p>
     <h3>Section 1a</h3>
     <p>
-      I'm baby photo booth ennui pabst YOLO, organic migas trust fund gochujang sartorial wolf. Cold-pressed sus
-      tumeric, vaporware palo santo next level succulents church-key bicycle rights intelligentsia single-origin coffee.
-      Freegan ethical PBR&B tumblr. Twee af stumptown marxism umami. Yuccie ethical cronut jawn fit pug photo booth
-      flannel neutral milk hotel marxism raw denim. Leggings actually PBR&B air plant try-hard tumblr, seitan celiac
-      same cliche hella hammock ramps.
+      Section 1a digs deeper into the following things. I'm baby photo booth ennui pabst YOLO, organic migas trust fund
+      gochujang sartorial wolf. Cold-pressed sus tumeric, vaporware palo santo next level succulents church-key bicycle
+      rights intelligentsia single-origin coffee. Freegan ethical PBR&B tumblr. Twee af stumptown marxism umami. Yuccie
+      ethical cronut jawn fit pug photo booth flannel neutral milk hotel marxism raw denim. Leggings actually PBR&B air
+      plant try-hard tumblr, seitan celiac same cliche hella hammock ramps.
     </p>
     <h3 id='section-1b.2'>Section 1b</h3>
-    <p>I'm baby photo booth ennui pabst YOLO, organic migas trust fund gochujang sartorial wolf.</p>
+    <p>Section 1b contains a list of things.</p>
     <ul>
       <li>Cold-pressed sus tumeric</li>
       <li>Next level succulents</li>
@@ -29,19 +29,19 @@ export const docsSampleText = (
     </ul>
     <h2>Section 2</h2>
     <p>
-      I'm baby photo booth ennui pabst YOLO, organic migas trust fund gochujang sartorial wolf. Cold-pressed sus
-      tumeric, vaporware palo santo next level succulents church-key bicycle rights intelligentsia single-origin coffee.
-      Freegan ethical PBR&B tumblr. Twee af stumptown marxism umami. Yuccie ethical cronut jawn fit pug photo booth
-      flannel neutral milk hotel marxism raw denim. Leggings actually PBR&B air plant try-hard tumblr, seitan celiac
-      same cliche hella hammock ramps.
+      Section 2 is about the following things. I'm baby photo booth ennui pabst YOLO, organic migas trust fund gochujang
+      sartorial wolf. Cold-pressed sus tumeric, vaporware palo santo next level succulents church-key bicycle rights
+      intelligentsia single-origin coffee. Freegan ethical PBR&B tumblr. Twee af stumptown marxism umami. Yuccie ethical
+      cronut jawn fit pug photo booth flannel neutral milk hotel marxism raw denim. Leggings actually PBR&B air plant
+      try-hard tumblr, seitan celiac same cliche hella hammock ramps.
     </p>
     <h2 id='section-3'>Section 3</h2>
     <p>
-      I'm baby photo booth ennui pabst YOLO, organic migas trust fund gochujang sartorial wolf. Cold-pressed sus
-      tumeric, vaporware palo santo next level succulents church-key bicycle rights intelligentsia single-origin coffee.
-      Freegan ethical PBR&B tumblr. Twee af stumptown marxism umami. Yuccie ethical cronut jawn fit pug photo booth
-      flannel neutral milk hotel marxism raw denim. Leggings actually PBR&B air plant try-hard tumblr, seitan celiac
-      same cliche hella hammock ramps.
+      And finally, Section 3 is about the following things. I'm baby photo booth ennui pabst YOLO, organic migas trust
+      fund gochujang sartorial wolf. Cold-pressed sus tumeric, vaporware palo santo next level succulents church-key
+      bicycle rights intelligentsia single-origin coffee. Freegan ethical PBR&B tumblr. Twee af stumptown marxism umami.
+      Yuccie ethical cronut jawn fit pug photo booth flannel neutral milk hotel marxism raw denim. Leggings actually
+      PBR&B air plant try-hard tumblr, seitan celiac same cliche hella hammock ramps.
     </p>
     <p>
       I'm baby photo booth ennui pabst YOLO, organic migas trust fund gochujang sartorial wolf. Cold-pressed sus
