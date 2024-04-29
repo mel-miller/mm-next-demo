82356beffdf38c659de700630d0952a1c227b20c
M Miller <melissa.miller@pantheon.io>
2024-04-29 13:42:52 -0700

    testing sidebar menu

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -4,25 +4,30 @@ import React from 'react';
 
 import {
   SidebarLayout,
+  SideNav,
   TableOfContents,
 } from '@pantheon-systems/pds-toolkit-react';
 
 import { docsSampleText } from './docs-sample-text';
+import { sideNavMenuItems } from '../../data/side-navigation.js';
 
 export default function Documentation() {
   return (
     <>
-      <h1>Documentation</h1>
-      <SidebarLayout>
-        <article slot='content' id='pds-toc-source' className='docs-content'>
-          {docsSampleText}
-        </article>
-        <TableOfContents
-          slot='sidebar'
-          headingText='Document contents'
-          showHeading={true}
-          appendIds={true}
-        />
+      <SidebarLayout sidebarLocation='left'>
+        <SideNav slot='sidebar' menuItems={sideNavMenuItems} />
+        <SidebarLayout slot='content'>
+          <h1>Documentation</h1>
+          <article slot='content' id='pds-toc-source' className='docs-content'>
+            {docsSampleText}
+          </article>
+          <TableOfContents
+            slot='sidebar'
+            headingText='Document contents'
+            showHeading={true}
+            appendIds={true}
+          />
+        </SidebarLayout>
       </SidebarLayout>
     </>
   );
---
+++ src/data/side-navigation.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Link from 'next/link';
+
+export const sideNavMenuItems = [
+  { linkContent: <Link href='/about'>About</Link> },
+  { linkContent: <Link href='/docs'>Docs</Link> },
+  { linkContent: <Link href='/docs2'>Docs2</Link> },
+];
