7023fa9580b13adb49214fc04ecc66251c8a2b99
M Miller <melissa.miller@pantheon.io>
2024-08-21 10:09:41 -0700

    Update ComponentTesting.tsx
    Remove toaster

--- src/app/about/ComponentTesting.tsx
+++ src/app/about/ComponentTesting.tsx
@@ -1,12 +1,7 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 
-import {
-  Button,
-  MenuButton,
-  SplitButton,
-  Toaster,
-} from '@pantheon-systems/pds-toolkit-react';
+import { MenuButton, SplitButton } from '@pantheon-systems/pds-toolkit-react';
 
 const sampleActionItems = [
   {
@@ -38,39 +33,10 @@ const categoryMenuItems = [
 ];
 
 export const ComponentTesting = () => {
-  const toastDuration = 3000;
-
-  const [toastContent, setToastContent] = useState(null);
-  const successToast = (
-    <Toaster
-      toasts={[
-        {
-          id: '1',
-          type: 'success',
-          message: 'Success! Your changes have been saved.',
-          autoDismiss: true,
-          autoDismissTimeout: toastDuration,
-          onDismiss: () => {
-            console.log('success');
-          },
-        },
-      ]}
-    />
-  );
-
-  const fireToast = () => {
-    setToastContent(successToast);
-
-    setTimeout(() => {
-      setToastContent(null);
-    }, toastDuration + 1000);
-  };
-
   return (
     <div>
       <h2>Testing these components</h2>
       <div className='pds-button-group'>
-        <Button label='Toast' variant='primary' onClick={fireToast} />
         <SplitButton variant='secondary' actionItems={sampleActionItems} />
         <MenuButton
           label='Filter by category'
@@ -78,7 +44,6 @@ export const ComponentTesting = () => {
           menuItems={categoryMenuItems}
         />
       </div>
-      {toastContent}
     </div>
   );
 };
